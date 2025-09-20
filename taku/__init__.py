b3e338f5dcb162dea8d70787544b6f9c47dce230
Tobi-De <tobidegnon@proton.me>
2025-09-20 09:10:15 +0100

    Bump version: 0.3.1 → 0.4.0

--- taku/__init__.py
+++ taku/__init__.py
@@ -48,7 +48,7 @@ parser.add_argument(
     default=default_scripts_dir,
     help=f"Scripts directory, default to {default_scripts_dir}",
 )
-parser.add_argument("--version", action="version", version="%(prog)s 0.3.1")
+parser.add_argument("--version", action="version", version="%(prog)s 0.4.0")
 subparsers = parser.add_subparsers(dest="command", required=True)
 
 cmd = command(subparsers)
